6df2d4aba2af44eed282a2f37b993f8a4178d64f
DenysShynkorenko887-eng <DenysShynkorenko887@icloud.com>
2025-12-04 10:44:29 +0200

    Delete GesturePlaybook directory

--- GesturePlaybook/GesturePlaybook/neonGlow.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension View {
-    func neonGlow(_ color: Color, radius: CGFloat = 12, intensity: Double = 0.6) -> some View {
-        self
-            .shadow(color: color.opacity(intensity), radius: radius, x: 0, y: 0)
-            .shadow(color: color.opacity(intensity/2), radius: radius/2, x: 0, y: 0)
-    }
-}
--- GesturePlaybook/Pods/Target Support Files/Pods-GesturePlaybook/Pods-GesturePlaybook-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GesturePlaybook : NSObject
-@end
-@implementation PodsDummy_Pods_GesturePlaybook
-@end
